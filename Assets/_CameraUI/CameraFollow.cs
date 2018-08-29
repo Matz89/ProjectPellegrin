cae1c9cb30649e156619b82abcdf0b64a34a1ada
Matthew P <matthewbpelletier@gmail.com>
2018-08-29 15:38:02 -0400

    Showcase and Test Level scenes. SpritMan3d, Ground Textures pack, and Cinemachine standard assets used.  Camera and level blocking.

---
+++ Assets/_CameraUI/CameraFollow.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    [SerializeField] GameObject followTarget;
+	
+	void LateUpdate () {
+        transform.position = followTarget.transform.position;
+	}
+}
